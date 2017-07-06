0ee7951a6ff85a4acb0c2e96bec60cd57c84cdf7
Pranav Krishnamurthy <pkrish19@outlook.com>
2017-07-06 17:05:27 -0400

    Adding more of the necessary structures
    All of the latest changes are done

---
+++ Assets/Scripts/GameDescriptionBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameDescriptionBehavior : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Hit!"); 
+    }
+
+    public void progressTrigger(int ind)
+    {
+        switch (ind)
+        {
+            case (0):
+                SceneManager.LoadScene("Main");
+                break;
+            case (1):
+                SceneManager.LoadScene("Menu");
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
