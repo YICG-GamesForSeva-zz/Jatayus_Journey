d7b3273e574e2e99d1e160f3a3d2f172b29ee5a8
Pranav Krishnamurthy <pkrish19@outlook.com>
2017-07-08 11:26:02 -0400

    Having Jatayu correctly scaled
    Right now ensuring that the various preparations are being made for the
    animation

---
+++ Assets/Scripts/JatayuMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JatayuMenuController : MonoBehaviour
+{
+
+    private Animator anim; // Having the animation reference
+    private Rigidbody2D rbJatayuMenu; // Creating the reference to the Rigidbody2D component
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
